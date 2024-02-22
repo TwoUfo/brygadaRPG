c3605d50eb1ab89479b62921e9d24d2ce3595e57
TwoUfo <94078255+TwoUfo@users.noreply.github.com>
2024-02-22 22:26:51 +0200

    player edited

--- brygada/Assets/Sctipts/PlayerMove.cs
+++ brygada/Assets/Sctipts/PlayerMove.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 5f; // Ўвидк≥сть руху гравц€
+    public float speed = 5f;
+    PhotonView view;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
 
     void Update()
     {
-        // ќтримуЇмо значенн€ в≥джет≥в управл≥нн€
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-
-        // –ух гравц€
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f) * speed * Time.deltaTime;
-        transform.position += movement;
+        if(view.IsMine)
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
+            transform.position += (Vector3)moveAmount;
+        }
     }
 }
